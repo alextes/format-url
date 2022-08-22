874ce5eed1f10647a4243d50f22ca2a21a10452c
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-22 19:50:31 +0200

    docs: add documentation to all public functions

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@
 use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 
 type SubstitutePairs<'a> = Vec<(&'a str, &'a str)>;
+type QueryParams<'a> = Vec<(&'a str, &'a str)>;
 
 fn strip_double_slash<'a>(base_url: &str, route_template: &'a str) -> &'a str {
     if base_url.ends_with("/") && route_template.starts_with("/") {
@@ -40,45 +41,39 @@ fn format_path(route_template: &str, substitutes: &SubstitutePairs) -> String {
         })
 }
 
+fn naive_encode_query_string<'a>(query_params: &QueryParams<'a>) -> String {
+    let query_string = query_params
+        .iter()
+        .map(|(key, value)| {
+            format!(
+                "{}={}",
+                utf8_percent_encode(key, NON_ALPHANUMERIC),
+                utf8_percent_encode(value, NON_ALPHANUMERIC)
+            )
+        })
+        .collect::<Vec<String>>()
+        .join("&");
+
+    "?".to_string() + (&query_string)
+}
+
+/// A collection of all the components and configuration that together serialize into a URL.
 pub struct FormatUrl<'a> {
     base: &'a str,
     disable_encoding: bool,
     path_template: Option<&'a str>,
-    query_params: Option<SubstitutePairs<'a>>,
+    query_params: Option<QueryParams<'a>>,
     substitutes: Option<SubstitutePairs<'a>>,
 }
 
 impl<'a> FormatUrl<'a> {
-    pub fn new(base: &'a str) -> Self {
-        Self {
-            base,
-            disable_encoding: false,
-            path_template: None,
-            query_params: None,
-            substitutes: None,
-        }
-    }
-
-    pub fn with_path_template(mut self, path_template: &'a str) -> Self {
-        self.path_template = Some(path_template);
-        self
-    }
-
-    pub fn with_query_params(mut self, params: SubstitutePairs<'a>) -> Self {
-        self.query_params = Some(params);
-        self
-    }
-
-    pub fn with_substitutes(mut self, substitutes: SubstitutePairs<'a>) -> Self {
-        self.substitutes = Some(substitutes);
-        self
-    }
-
+    /// In rare cases you may need the query parameter key/value pairs not to be encoded.
     pub fn disable_encoding(mut self) -> Self {
         self.disable_encoding = true;
         self
     }
 
+    /// Takes all of the provided arguments and turns them into a single URL to fetch.
     pub fn format_url(self) -> String {
         let formatted_path = match (self.path_template, &self.substitutes) {
             (Some(path_template), Some(substitutes)) => format_path(path_template, &substitutes),
@@ -89,20 +84,7 @@ impl<'a> FormatUrl<'a> {
         let formatted_querystring = &self.query_params.map_or_else(
             || String::new(),
             |query_params| match self.disable_encoding {
-                false => {
-                    let query_string = query_params
-                        .iter()
-                        .map(|(key, value)| {
-                            format!(
-                                "{}={}",
-                                utf8_percent_encode(key, NON_ALPHANUMERIC),
-                                utf8_percent_encode(value, NON_ALPHANUMERIC)
-                            )
-                        })
-                        .collect::<Vec<String>>()
-                        .join("&");
-                    "?".to_string() + (&query_string)
-                }
+                false => naive_encode_query_string(&query_params),
                 true => {
                     let query_string = query_params
                         .iter()
@@ -120,6 +102,35 @@ impl<'a> FormatUrl<'a> {
             "{}{}{}",
             self.base, safe_formatted_route, formatted_querystring
         )
+    }
+
+    /// Start building a URL. The minimum required is some hostname.
+    pub fn new(base: &'a str) -> Self {
+        Self {
+            base,
+            disable_encoding: false,
+            path_template: None,
+            query_params: None,
+            substitutes: None,
+        }
+    }
+
+    /// Add a path, optionally marking sections for substitution using `:key`.
+    pub fn with_path_template(mut self, path_template: &'a str) -> Self {
+        self.path_template = Some(path_template);
+        self
+    }
+
+    /// Add some query parameters.
+    pub fn with_query_params(mut self, params: QueryParams<'a>) -> Self {
+        self.query_params = Some(params);
+        self
+    }
+
+    /// Add substitutes to substitute matching `:key` sequences in the path template.
+    pub fn with_substitutes(mut self, substitutes: SubstitutePairs<'a>) -> Self {
+        self.substitutes = Some(substitutes);
+        self
     }
 }
 
